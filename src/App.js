a5d24c99a67c71268bfd803306db7194ee01c0e7
streetlightkids <ltj_hq@hotmail.com>
2022-09-21 17:50:24 -0700

    App()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
 function App() {
+  const [taskList, setTaskList] = React.useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskForm
+        taskPropsCallback={(task) => setTaskList([task, ...taskList])}
+      />
+      <TaskList tasks={taskList} />
     </div>
   );
 }
